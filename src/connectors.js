2a7f0ac1158332d1bc803f26996fd4f3f44d47d1
Yury <yury13t@gmail.com>
2019-01-09 18:19:16 +0200

    first commit

---
+++ src/connectors.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        "id": 963751069051,
+        "name": "55A2-1610",
+        "description": "'5500 Series, Attachable, Sz 16, 10 contact, Dry Mate Receptacle'",
+        "pressure_rating": "10K",
+        "voltage_rating": 600,
+        "wiregauge": 20,
+        "contacts": "10",
+        "jpeg_file_url": "/converters.jpg",
+        "gender": "Male"
+    },
+    {
+        "id": 963751069052,
+        "name": "55A2-1610",
+        "description": "'5500 Series, Attachable, Sz 16, 10 contact, Dry Mate Receptacle'",
+        "pressure_rating": "10K",
+        "voltage_rating": 600,
+        "wiregauge": 20,
+        "contacts": "10",
+        "jpeg_file_url": "/converters.jpg",
+        "gender": "Male"
+    },
+    {
+        "id": 963751069053,
+        "name": "55A2-1610",
+        "description": "'5500 Series, Attachable, Sz 16, 10 contact, Dry Mate Receptacle'",
+        "pressure_rating": "10K",
+        "voltage_rating": 600,
+        "wiregauge": 20,
+        "contacts": "10",
+        "jpeg_file_url": "/converters.jpg",
+        "gender": "Male"
+    },
+    {
+        "id": 963751069054,
+        "name": "55A2-1610",
+        "description": "'5500 Series, Attachable, Sz 16, 10 contact, Dry Mate Receptacle'",
+        "pressure_rating": "10K",
+        "voltage_rating": 600,
+        "wiregauge": 20,
+        "contacts": "10",
+        "jpeg_file_url": "/converters.jpg",
+        "gender": "Male"
+    },
+    {
+        "id": 963751069055,
+        "name": "55A2-1610",
+        "description": "'5500 Series, Attachable, Sz 16, 10 contact, Dry Mate Receptacle'",
+        "pressure_rating": "10K",
+        "voltage_rating": 600,
+        "wiregauge": 20,
+        "contacts": "10",
+        "jpeg_file_url": "/converters.jpg",
+        "gender": "Male"
+    }
+];
